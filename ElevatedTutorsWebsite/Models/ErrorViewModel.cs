ec4cf55c21a489e7ab5f3cc459a7b721a4460935
Nicholas Liam Wolfaardt <129847542+ST10273397@users.noreply.github.com>
2025-10-30 22:07:25 +0200

    Delete ElevatedTutorsWebsite directory

--- ElevatedTutorsWebsite/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ElevatedTutorsWebsite.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
